e6b9f9ad2f71d7945bfa5a567043e45f4603dfe0
Stephan Meißner <stephan.meissner@dccs.at>
2019-05-02 19:11:21 +0200

    Made work with hooks.

--- src/DataGridMui.tsx
+++ src/DataGridMui.tsx
@@ -10,16 +10,18 @@ import SnackbarContent from "@material-ui/core/SnackbarContent";
 import Button from "@material-ui/core/Button/Button";
 import ErrorIcon from "@material-ui/icons/Error";
 
+export type OnLoadData = (
+  page: number,
+  rowsPerPage: number,
+  orderBy: string,
+  desc: boolean,
+  filter: { [key: string]: any }
+) => Promise<{ total: number; data: any[] }>;
+
 interface IProps {
   colDef: IColDef[];
   initRowsPerPage?: number;
-  onLoadData: (
-    page: number,
-    rowsPerPage: number,
-    orderBy: string,
-    desc: boolean,
-    filter: { [key: string]: any }
-  ) => Promise<{ total: number; data: any[] }>;
+  onLoadData: OnLoadData;
   disablePaging?: boolean;
   onRowClick?: (data: any) => void;
   subComponent?: (data: any) => React.ReactNode;
@@ -29,93 +31,131 @@ interface IState {
   rowsPerPage: number;
   page: number;
   total: number;
-  data: any[];
-  loading: boolean;
-  error: boolean;
   orderBy?: IColDef;
   desc: boolean;
   filter: any;
+  reloadDummy: boolean;
 }
 
-export class DataGridMui extends React.Component<IProps, IState> {
-  state = {
-    rowsPerPage: 10,
-    page: 0,
-    total: 0,
-    data: [],
-    loading: false,
-    error: false,
-    orderBy: undefined,
-    desc: false,
-    filter: {}
-  };
+export interface IDataGridStateContext {
+  state: IState;
+  dispatch: React.Dispatch<IAction>;
+}
 
-  componentDidMount() {
-    if (this.props.initRowsPerPage) {
-      this.setState({ rowsPerPage: this.props.initRowsPerPage }, this.load);
-    } else {
-      this.load();
-    }
+export const DataGridState = React.createContext<
+  IDataGridStateContext | undefined
+>(undefined);
+
+type ReducerActionType =
+  | "set-total"
+  | "set-page"
+  | "set-rowsperpage"
+  | "set-orderBy"
+  | "set-filter"
+  | "reload";
+
+interface IAction {
+  type: ReducerActionType;
+  payload: any;
+}
+
+function reducer(s: any, action: { type: ReducerActionType; payload: any }) {
+  switch (action.type) {
+    case "set-total":
+      return { ...s, total: action.payload };
+    case "set-page":
+      return { ...s, page: action.payload };
+    case "set-rowsperpage":
+      return { ...s, rowsPerPage: action.payload };
+    case "set-orderBy":
+      return { ...s, ...action.payload };
+    case "set-filter":
+      return { ...s, filter: { ...s.filter, ...action.payload } };
+    case "reload":
+      return { ...s, reloadDummy: !s.reloadDummy };
+
+    default:
+      throw new Error();
+  }
+}
+
+export function DataGridStateProvider({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  const [state, dispatch] = React.useReducer<React.Reducer<IState, IAction>>(
+    reducer,
+    createDefaultState()
+  );
+
+  return (
+    <DataGridState.Provider value={{ state, dispatch }}>
+      {children}
+    </DataGridState.Provider>
+  );
+}
+
+export function DataGridMui(props: IProps) {
+  // Do we have a custom context,...
+  let stateContext = React.useContext(DataGridState);
+  const privateReducer = React.useReducer<React.Reducer<IState, IAction>>(
+    reducer,
+    createDefaultState()
+  ); // ...if not, we need to create the state.
+
+  if (stateContext === undefined) {
+    stateContext = { state: privateReducer[0], dispatch: privateReducer[1] };
   }
 
-  render() {
-    if (this.state.error) {
-      return this.renderError();
-    }
+  const { state, dispatch } = stateContext!;
+  const [data, setData] = React.useState<any[]>([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
 
-    return (
-      <Paper style={{ position: "relative", overflowX: "auto", width: "100%" }}>
-        {this.state.loading && (
-          <ProgressWrapper>
-            <CircularProgress />
-          </ProgressWrapper>
-        )}
+  React.useEffect(() => {
+    setLoading(true);
+    setError(false);
 
-        <TablePlain
-          {...tableMuiTheme}
-          data={this.state.data}
-          colDef={this.props.colDef}
-          orderedBy={this.state.orderBy}
-          desc={this.state.desc}
-          onChangeOrderBy={this.handleChangeOrderBy}
-          onChangeFilter={this.handleChangeFilter}
-          onRowClick={this.props.onRowClick}
-          subComponent={this.props.subComponent}
-        />
+    props
+      .onLoadData(
+        state.page + 1,
+        state.rowsPerPage,
+        state.orderBy ? (state.orderBy! as IColDef).prop : "",
+        state.desc,
+        state.filter
+      )
+      .then(({ data: d, total }) => {
+        dispatch({ type: "set-total", payload: total });
+        setData(d);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+        setError(true);
+      });
+  }, [
+    state.rowsPerPage,
+    state.page,
+    state.orderBy,
+    state.desc,
+    state.filter,
+    state.reloadDummy,
+    props,
+    dispatch
+  ]);
 
-        {this.props.disablePaging !== true && (
-          <TablePagination
-            component={props => <div {...props}>{props.children}</div>}
-            colSpan={this.props.colDef != null ? this.props.colDef.length : 1}
-            count={this.state.total}
-            rowsPerPage={this.state.rowsPerPage}
-            page={this.state.page}
-            onChangePage={(e, p) => this.handleChangePage(p)}
-            onChangeRowsPerPage={e =>
-              this.handleChangeRowsPerPage(parseInt(e.target.value, 10))
-            }
-            labelRowsPerPage={"Einträge pro Seite:"}
-            labelDisplayedRows={({ from, to, count }) =>
-              `${from}-${to} von ${count}`
-            }
-          />
-        )}
-      </Paper>
-    );
-  }
-
-  renderError() {
+  function renderError() {
     return (
       <SnackbarContent
         style={{ width: "100%" }}
         message={
           <ErrorMessage>
-            <ErrorIcon />
-            Die Daten konnten nicht geladen werden.
+            <ErrorIcon /> Die Daten konnten nicht geladen werden.
           </ErrorMessage>
         }
         action={
-          <Button onClick={() => this.load()} color="primary" size="small">
+          <Button onClick={() => load()} color="primary" size="small">
             Neu laden
           </Button>
         }
@@ -123,55 +163,94 @@ export class DataGridMui extends React.Component<IProps, IState> {
     );
   }
 
-  load() {
-    this.setState({ loading: true, error: false });
-    this.props
+  function load() {
+    setLoading(true);
+    setError(false);
+
+    props
       .onLoadData(
-        this.state.page + 1,
-        this.state.rowsPerPage,
-        this.state.orderBy ? (this.state.orderBy! as IColDef).prop : "",
-        this.state.desc,
-        this.state.filter
+        state.page + 1,
+        state.rowsPerPage,
+        state.orderBy ? (state.orderBy! as IColDef).prop : "",
+        state.desc,
+        state.filter
       )
-      .then(({ data, total }) =>
-        this.setState({
-          data,
-          total,
-          loading: false
-        })
-      )
+      .then(({ data: d, total }) => {
+        dispatch({ type: "set-total", payload: total });
+        setData(d);
+        setLoading(false);
+      })
       .catch(() => {
-        this.setState({ loading: false, error: true });
+        setLoading(false);
+        setError(true);
       });
   }
 
-  handleChangePage = (page: number) => {
-    this.setState({ page }, this.load);
-  };
+  function handleChangePage(page: number) {
+    dispatch({ type: "set-page", payload: page });
+  }
 
-  handleChangeRowsPerPage = (rows: number) => {
-    this.setState({ rowsPerPage: rows }, this.load);
-  };
+  function handleChangeRowsPerPage(rows: number) {
+    dispatch({ type: "set-rowsperpage", payload: rows });
+  }
 
-  handleChangeOrderBy = (colDef: IColDef) => {
+  function handleChangeOrderBy(colDef: IColDef) {
     let desc = false;
-    if (this.state.orderBy === colDef) {
-      desc = !this.state.desc;
-    }
-    this.setState({ orderBy: colDef, desc }, this.load);
-  };
 
-  handleChangeFilter = (colDef: IColDef, value: any) => {
-    this.setState(
-      p => ({
-        filter: {
-          ...p.filter,
-          [colDef.prop]: value
-        }
-      }),
-      this.load
-    );
-  };
+    if (state.orderBy === colDef) {
+      desc = !state.desc;
+    }
+
+    dispatch({ type: "set-orderBy", payload: { orderBy: colDef, desc } });
+  }
+
+  function handleChangeFilter(colDef: IColDef, value: any) {
+    dispatch({ type: "set-filter", payload: { [colDef.prop]: value } });
+  } // ------------ // Render
+
+  if (error) {
+    return renderError();
+  }
+
+  return (
+    <Paper style={{ position: "relative", overflowX: "auto", width: "100%" }}>
+      {loading && (
+        <ProgressWrapper>
+          <CircularProgress />
+        </ProgressWrapper>
+      )}
+
+      <TablePlain
+        {...tableMuiTheme}
+        data={data}
+        colDef={props.colDef}
+        orderedBy={state.orderBy}
+        desc={state.desc}
+        onChangeOrderBy={handleChangeOrderBy}
+        onChangeFilter={handleChangeFilter}
+        onRowClick={props.onRowClick}
+        subComponent={props.subComponent}
+      />
+
+      {props.disablePaging !== true && (
+        <TablePagination
+          component={ps => <div {...ps}>{ps.children}</div>}
+          colSpan={props.colDef != null ? props.colDef.length : 1}
+          count={state.total}
+          rowsPerPage={state.rowsPerPage}
+          page={state.page}
+          onChangePage={(e, p) => handleChangePage(p)}
+          onChangeRowsPerPage={e =>
+            handleChangeRowsPerPage(parseInt(e.target.value, 10))
+          }
+          labelRowsPerPage={"Einträge pro Seite:"}
+          labelDisplayedRows={({ from, to, count }) =>
+            `${from}-${to} von ${count}`
+          }
+        />
+      )}
+    </Paper>
+  );
 }
 
 const ProgressWrapper = styled.div`
@@ -189,8 +268,19 @@ const ProgressWrapper = styled.div`
 const ErrorMessage = styled.span`
   display: flex;
   align-items: center;
-
   svg {
     margin-right: 0.5rem;
   }
 `;
+
+function createDefaultState(): IState {
+  return {
+    rowsPerPage: 10,
+    page: 0,
+    total: 0,
+    orderBy: undefined,
+    desc: false,
+    filter: {},
+    reloadDummy: false
+  };
+}
