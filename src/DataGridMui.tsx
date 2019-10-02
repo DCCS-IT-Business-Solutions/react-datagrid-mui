3212d21d2dfc0dd921eafcae9e730142a411be33
smeissner <stephan.meissner@dccs.at>
2019-10-02 12:05:58 +0200

    #5 removed styled-components

--- src/DataGridMui.tsx
+++ src/DataGridMui.tsx
@@ -1,22 +1,43 @@
 import * as React from "react";
-import styled from "styled-components";
 import { TablePlain, ITablePlainProps } from "@dccs/react-table-plain";
 import { tableMuiTheme } from "@dccs/react-table-mui";
 import { IRenderPagingProps } from "@dccs/react-datagrid-plain";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import TablePagination from "@material-ui/core/TablePagination";
-import SnackbarContent from "@material-ui/core/SnackbarContent";
-import Button from "@material-ui/core/Button/Button";
+import {
+  CircularProgress,
+  TablePagination,
+  SnackbarContent,
+  Button
+} from "@material-ui/core";
 import ErrorIcon from "@material-ui/icons/Error";
+
+const progressWrapper = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  background: "rgba(255, 255, 255, 0.5)"
+} as React.CSSProperties;
+
+const errorMessage = {
+  display: "flex",
+  alignItems: "center",
+  svg: {
+    marginRight: "0.5rem"
+  }
+} as React.CSSProperties;
 
 function renderError(load: any) {
   return (
     <SnackbarContent
       style={{ width: "100%", boxSizing: "border-box" }}
       message={
-        <ErrorMessage>
+        <div style={errorMessage}>
           <ErrorIcon /> Die Daten konnten nicht geladen werden.
-        </ErrorMessage>
+        </div>
       }
       action={
         <Button onClick={() => load()} color="primary" size="small">
@@ -32,9 +53,9 @@ export const datagridMuiTheme = {
     <TablePlain {...tableMuiTheme} {...ps} />
   ),
   renderLoading: () => (
-    <ProgressWrapper>
+    <div style={progressWrapper}>
       <CircularProgress />
-    </ProgressWrapper>
+    </div>
   ),
   renderError,
   renderPaging: ({
@@ -58,23 +79,3 @@ export const datagridMuiTheme = {
     />
   )
 };
-
-const ProgressWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgba(255, 255, 255, 0.5);
-`;
-
-const ErrorMessage = styled.span`
-  display: flex;
-  align-items: center;
-  svg {
-    margin-right: 0.5rem;
-  }
-`;
